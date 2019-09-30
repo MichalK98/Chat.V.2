1a42391501695d6bd7d5b6bbf2d8c144cde48592
Michal K <michal10203040@gmail.com>
2019-09-30 15:57:23 +0200

    ADD_MESSAGE, just a start ;)

--- src/components/Submit/Submit.js
+++ src/components/Submit/Submit.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // SVG
 import SendSvg from '../../svg/send.svg';
 
 class Submit extends Component {
+    handleChange = (e) => {
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+        
+        this.props.addMessage('hello');
+    }
+
     render() {
+        console.log(this.props);
         return (
             <div className="chat-footer">
-                <form autoComplete="off">
-                    <input type="text" placeholder="Skriv något..."/>
+                <form onSubmit={this.handleSubmit} autoComplete="off">
+                    <input  onChange={this.handleChange} value={this.props.message} type="text" placeholder="Skriv något..."/>
                     <button id="btnSend"><SendSvg/></button>
                 </form>
             </div>
         )
     }
 }
-export default Submit;
+
+const mapStateToProps = (state) => {
+    return {
+        messages: state.messages
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addMessage: (message) => { dispatch({type: 'ADD_MESSAGE', message: message})}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Submit);
