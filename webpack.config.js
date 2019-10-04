1e998742b052a678214743839dd5b20fca5864b8
Michal K <michal10203040@gmail.com>
2019-10-04 11:07:17 +0200

    Sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,13 @@ module.exports = {
                 "css-loader"
             ]
         }, {
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader"
+            ]
+        }, {
             test: /\.svg$/,
             use: [
                 "@svgr/webpack"
